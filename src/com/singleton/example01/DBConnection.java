f0ee0365c6724c254ebe654666f95236de013634
MadhushaPrasad <madushaprasad21@gmail.com>
2021-10-22 23:23:50 +0530

    feat (singleton design pattern): create singleton design pattern example one

---
+++ src/com/singleton/example01/DBConnection.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.singleton.example01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Madhusha Prasad
+ */
+public class DBConnection {
+
+    private static DBConnection dBConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "", "");
+    }
+
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (dBConnection == null) {
+            return new DBConnection();
+        }
+        return dBConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
